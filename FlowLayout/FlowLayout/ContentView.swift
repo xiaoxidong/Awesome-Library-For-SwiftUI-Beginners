c73f8e8c4819b6a616e6fbb5579a1d791124682d
Xiaodong <834599524@qq.com>
2023-12-07 12:17:00 +0800

    init

---
+++ FlowLayout/FlowLayout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowLayout
+//
+//  Created by XiaoDong Yuan on 2023/12/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowLayout/FlowLayout/FlowLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowLayoutApp.swift
+//  FlowLayout
+//
+//  Created by XiaoDong Yuan on 2023/12/7.
+//
+
+import SwiftUI
+
+@main
+struct FlowLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
