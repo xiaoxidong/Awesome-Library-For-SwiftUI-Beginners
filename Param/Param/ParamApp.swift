f8f8f1694328b3bc72e81457180f78ac84b5756c
Xiaodong <834599524@qq.com>
2023-12-07 12:16:07 +0800

    New

---
+++ Param/Param/ParamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParamApp.swift
+//  Param
+//
+//  Created by XiaoDong Yuan on 2023/12/7.
+//
+
+import SwiftUI
+
+@main
+struct ParamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
